7d6a061a38bfc2d12946bd6d70d6682b074d7b41
Supharoek Sudadet <supharoek.0609@gmail.com>
2021-07-28 03:01:16 +0700

    client optimization

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -26,7 +26,7 @@ ready(function() {
             codename: codename,
             affiliation: affiliation,
             message: umessage
-        });
+        })
         swal.fire({
             title: 'Success!',
             text: 'ส่งข้อความเรียบร้อย',
@@ -62,16 +62,17 @@ function sendMessage(data) {
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'same-origin', // include, *same-origin, omit
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
             // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         redirect: 'follow', // manual, *follow, error
         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(data) // body data type must match "Content-Type" header
     })
-        .then(function (response) {
-            return response.json(); // parses JSON response into native JavaScript objects
-        })
+    .then(function (response) {
+        return response; // parses JSON response into native JavaScript objects
+    })
 }
 
 $(document).ready(function () {
