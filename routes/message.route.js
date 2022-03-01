f02750bfae422471b7eba88d70db35198f1355b8
Supharoek Sudadet <supharoek.0609@gmail.com>
2022-03-01 15:30:09 +0700

    :art: :bug: improve structure of the code and fix a bug

--- routes/message.route.js
+++ routes/message.route.js
@@ -5,18 +5,32 @@ const controllers = require('../controllers/message.controller');
 router.get('/messages', async (req, res) => {
     try {
         let result = await controllers.getMessage();
-        res.send(result);
+        if(result.length === 0) {
+            return res.sendStatus(204);
+        } else {
+            return res.status(200).json(result);
+        }
+        
     } catch(error) {
         console.log(error.message);
     }
+    return;
 });
 
 router.post('/messages', async (req, res) => {
     try {
-        await controllers.postMessage(req);
+        let codename = req.body.codename;
+        let affiliation = req.body.affiliation;
+        let message = req.body.codename;
+        if (codename.length === 0 || affiliation.length === 0 || message.length === 0) {
+            res.sendStatus(400);
+        } else {
+            return await controllers.postMessage(req, res);
+        }
     } catch(error) {
         console.log(error.message);
     }
+    return;
 });
 
 module.exports = router;
